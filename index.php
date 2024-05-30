2c36998d38e04602a2a9c2ef0da2a79a0595a354
armandssss <113087300+armandssss@users.noreply.github.com>
2024-05-30 22:42:52 +0300

    update

--- index.php
+++ index.php
@@ -105,13 +105,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-<correct_hash_here>" crossorigin="anonymous">
-    <title>CityStore - Ļoti ērti lietojams un lietotājam draudzīgs interneta veikals</title>
+    <title>CityStore - Ļoti ērti lietojams un lietotājam draudzīgs testa interneta veikals</title>
     <meta name="description" content="Testa interneta veikals Citystore – ļoti ērti lietojams un lietotājam draudzīgs interneta veikals.">
     <meta name="keywords" content="iPhone 15 Pro, AirPods Pro, AirPods Max, Apple Watch, MacBook Pro, iPhone SE, iPad Air Pro, Apple Watch SE, AirPods 3, Mac Mini, Apple TV 4K, MacBook Air, Apple">
     <meta property="og:title" content="CityStore - citystore.kdv.lv">
     <meta property="og:url" content="https://citystore.kdv.lv/">
-    <meta property="og:image" content="citystore.png">
-    <meta property="og:description" content="Ļoti ērti lietojams un lietotājam draudzīgs interneta veikals.">
+    <meta property="og:image" content="/citystore.png">
+    <meta property="og:description" content="Ļoti ērti lietojams un lietotājam draudzīgs testa interneta veikals.">
 </head>
 <body>
     <div class="wrapper">
