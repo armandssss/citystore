e0e3f68197ef7e6fc014916ee3ed27ff9df98121
armandssss <113087300+armandssss@users.noreply.github.com>
2024-06-13 22:16:18 +0300

    update

--- checkout-success.php
+++ checkout-success.php
@@ -76,7 +76,7 @@ if (isset($_SESSION['user_id'])) {
     <div class="payment-success" style="color:white;">
         <h1>Payment Successful!</h1>
         <p>Your payment has been processed successfully. Thank you for your purchase!</p>
-        <div class="logout" style="align-items: center;">
+        <div class="logout" style="justify-content: center;">
         <strong><p><a href="/" style="color:white;" class="logout-btn">Go Back to Shop</a></p></strong>
         </div>
     </div>
